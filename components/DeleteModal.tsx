e182e6aca3902f6a1ffbab300541b3a1d7bce812
ShinoHJ <910.shino@gmail.com>
2024-08-25 13:45:53 +0800

    --update delmodal text

--- components/DeleteModal.tsx
+++ components/DeleteModal.tsx
@@ -61,17 +61,17 @@ const DeleteModal: React.FC<DeleteModalProps> = ({ onClose, isVisible, post, onD
         <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content">
             <div className='formArea'>
-              <h5 className='text-red01 text-center'>{post?.title}</h5>
-              <div className='text-center mt-4'>Are you sure you want to delete this post?</div>
+              {/* <h5 className='text-red01 text-center'>{post?.title}</h5> */}
+              <div className='text-center mt-4'>Are you sure you want to delete this post (<span className='text-red01'>{post?.title}</span>)?</div>
               <div className="row justify-content-center">
                 <div className="col-4">
                   <button type="button" className="btn btn-yellow02 mt-4 mb-3 rounded-pill w-100" onClick={onClose}>
-                    Cancel
+                    No
                   </button>
                 </div>
                 <div className="col-4">
                   <button type="button" className="btn btn-red01 mt-4 mb-3 rounded-pill w-100" onClick={handleRemovePost}>
-                    Delete
+                    Yes
                   </button>
                 </div>
               </div>
